cd7dabf8b25acb7dbc95e78fa3baae7c19df83c5
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2025-11-02 17:46:28 -0500

    Tauri scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { createSignal } from "solid-js";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+
+  return (
+    <main class="container">
+      <h1>hello world</h1>
+    </main>
+  );
+}
+
+export default App;
