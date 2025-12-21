3e1a9bd95443a2cd2e19de14f291dac59cf41f5a
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2025-12-20 21:50:55 -0500

    basic scaffolding of app!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { createSignal } from "solid-js";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import { Router, Route } from "@solidjs/router";
+import Dashboard from "./pages/Dashboard";
+import Todo from "./pages/Todo";
 
-function App() {
-
+export default function App() {
   return (
-    <main class="container">
-      <h1>hello world</h1>
+    <>
+    <main class="flex flex-row gap-5 p-4">
+      <div class="flex flex-col gap-2 border-r border-gray-300">
+        <a href="/">Dashboard</a> 
+        <a href="/todo">Todo List</a>
+      </div>
+      <Router>
+        <Route path="/" component={Dashboard} />
+        <Route path="/todo" component={Todo} />
+      </Router>
     </main>
+    </>
   );
 }
-
-export default App;
